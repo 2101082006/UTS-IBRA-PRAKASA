735526d74ffdde0f61e674dfedaaf9e37275b3b8
2101082006 <113502572+2101082006@users.noreply.github.com>
2022-11-15 10:06:30 +0700

    Add files via upload

---
+++ java/com/ibra/peminjaman/VO/ResponseTemplateVO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ibra.peminjaman.VO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsContructor
+public class ResponseTemplateVO {
+
+}
---
+++ java/com/ibra/peminjaman/repository/PeminjamanRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ibra.peminjaman.repository;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PeminjamanRepository {
+    
+}
